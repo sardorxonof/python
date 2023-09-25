380e4bdcd195e428383b6ee6368003b0d56536ba
sardor <sardorxonakobirov@gmail.com>
2023-09-25 20:29:52 +0300

    match_case_commit

--- my_first/main.py
+++ my_first/main.py
@@ -1,28 +1,47 @@
-import math
-import random
-# ceil = input("rondom number ceil !")
-# floor = input("rondom number floor !")
-# rounds = input("rondom number rounds !")
-# ------------------------------------------------------------------------------------------------------
-# print(math.ceil(float(ceil)),  end=" в большую сторону " )
-# print(math.floor(float(floor)), end=" в меньшую сторону ") 
-# print(round(float(rounds)), end=" в ровную сторону ") 
-# ------------------------------------------------------------------------------------------------------
-# print(random.randrange(1,10))
-# print(random.randint(1,10))
-# print(round(random.random() * 10))
-# ------------------------------------------------------------------------------------------------------
+# # import math
+# # import random
+# # ceil = input("rondom number ceil !")
+# # floor = input("rondom number floor !")
+# # rounds = input("rondom number rounds !")
+# # ------------------------------------------------------------------------------------------------------
+# # print(math.ceil(float(ceil)),  end=" в большую сторону " )
+# # print(math.floor(float(floor)), end=" в меньшую сторону ") 
+# # print(round(float(rounds)), end=" в ровную сторону ") 
+# # ------------------------------------------------------------------------------------------------------
+# # print(random.randrange(1,10))
+# # print(random.randint(1,10))
+# # print(round(random.random() * 10))
+# # ------------------------------------------------------------------------------------------------------
 
-input("rondom two numbers")
-one_number = int(input("one number"))
-two_numbre = int(input("two number"))
+# # input("rondom two numbers")
+# # one_number = int(input("one number"))
+# # two_numbre = int(input("two number"))
 
-numbers = random.randrange(one_number,two_numbre)
-# if numbers == 9: 
-#     print("first won" , numbers , end="!!!" , sep="_" )
-# elif numbers == 5:
-#     print("second won" , numbers , end="!!!" , sep="_" )
-# else :
-#     print("you not won")
+# # numbers = random.randrange(one_number,two_numbre)
+# # if numbers == 9: 
+# #     print("first won" , numbers , end="!!!" , sep="_" )
+# # elif numbers == 5:
+# #     print("second won" , numbers , end="!!!" , sep="_" )
+# # else :
+# #     print("you not won")
 
-print(numbers = random.randrange(one_number,two_numbre))
+# # print(numbers = random.randrange(one_number,two_numbre))
+
+# # lorem = input("lorem")
+
+# # # print(len(lorem))
+# # sardor = ["sardor",'help']
+# # match sardor:
+# #     case ["sardor",("sardor" | "help" | "rodras")]:
+# #         print("""lorem is lorem """)
+# #     case _:
+# #?         print("""lorem is not lorem """)
+
+# ! lorem = int(input())
+# ! match lorem:
+# !    case lorem if lorem == 10:
+# !        print("lorem is lorem ")
+# ?    case lorem if lorem == 1:
+# ?       print(1+2)
+# ?    case _:
+# ?        print("error lorem" )
