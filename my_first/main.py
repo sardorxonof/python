21cd6cd3319a91e0f3450144459ed59bb20d40cc
sardor <sardorxonakobirov@gmail.com>
2023-10-07 08:46:25 +0300

    peramid ex

--- my_first/main.py
+++ my_first/main.py
@@ -1,15 +1,22 @@
-def rotate_string_recursively(string ,number=1,number_9=0):
-    print(string)
-    string_3 = len(string) - int(number)
-    string_2 = string[-1:]
-    string_4 = string_2 + string[:string_3]
-    string_4 = str(string_4)
-    string_5 = len(string) - 1
-    number_9 = string_5
-    match number_9:
-        case 0:
-            print(string_4)
-        case _ :
-            rotate_string_recursively(string_4,number,number_9)
-rotate_string_recursively("Hello world")
+def piramid_recursion(n_times:int,number=0,number_2=0, number_3=0):
+    """piramid_recursion"""
+    if n_times % 2 == 0:
+        number += 2
+        lens = number * "*"
+        number_2 = n_times - number_3
+        if n_times == len(lens):
+            print(number_2 * " " , lens , number_2 * " " )
+        else:
+            print(number_2 * " " , lens , number_2 * " " )
+            piramid_recursion(n_times,number,number_2,number_3+1)
+    else :
+        number += 3
+        lens = number * "*"
+        number_2 = n_times - number_3
+        if n_times == len(lens):
+            print(number_2 * " " , lens , number_2 * " " )
+        else:
+            print(number_2 * " " , lens , number_2 * " " )
+            piramid_recursion(n_times,number,number_2,number_3+1)
 
+piramid_recursion(20)
